0f69b97c9d8d4e50ec356b77854d8d21a46a97fa
Luca Brianza <brianza@hercules.mib.infn.it>
2014-10-06 18:03:48 +0200

    update

---
+++ TB/compile.sh
@@ -0,0 +1,3 @@
+c++ -o calcTreshold bin/calcTreshold.cpp `root-config --cflags --glibs`;
+c++ -o analyzer bin/analyzer.cpp `root-config --cflags --glibs`;
+c++ -o dumper bin/dumper.cpp `root-config --cflags --glibs`;
