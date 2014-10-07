bd28720aa8375302fd3dc0f3f09bf8110025adcb
Luca Brianza <brianza@hercules.mib.infn.it>
2014-10-07 18:10:28 +0200

    draw waveform

---
+++ TB/python/drawWaveform.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import sys
+import os
+import commands
+from commands import getstatusoutput
+import datetime
+import argparse
+import string
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser (description = 'draw waveform: read raw data and draw some waveforms')
+    parser.add_argument ('-d', '--dir' , default = '/gwteray/users/marzocchi/iMCP/dataTrees', help='directory containing raw data')
+    parser.add_argument ('-n', '--chNumber' , default = '9', help='number of channels (in the raw data!)')
+    parser.add_argument ('-r', '--run' , default = '251', help='run to scan')
+    parser.add_argument ('-c', '--channel' , default = '0', help='channel to analyze')
+    parser.add_argument ('-f', '--firstEntry' , default = '0', help='first entry')
+    args = parser.parse_args ()
+    print './drawWaveform '+args.dir+' '+args.chNumber+' '+args.run+' '+args.channel+' '+args.firstEntry
+    os.system('./drawWaveform '+args.dir+' '+args.chNumber+' '+args.run+' '+args.channel+' '+args.firstEntry)
+    
