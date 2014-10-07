9532c90b8a5d1573b89b71f6aa66ae6c278aef6a
Luca Brianza <brianza@hercules.mib.infn.it>
2014-10-07 14:24:24 +0200

    check treshold

--- TB/src/MCPMap.cc
+++ TB/src/MCPMap.cc
@@ -7,7 +7,7 @@ void Fill_MCPList (void)
       MCPList.insert(std::make_pair("MiB2",2));
       MCPList.insert(std::make_pair("MiB3",3));
       MCPList.insert(std::make_pair("Planacon",4));
-      MCPList.insert(std::make_pair("Off1",5));
+      MCPList.insert(std::make_pair("Roma1",5));
       MCPList.insert(std::make_pair("Off2",6));
       MCPList.insert(std::make_pair("Off3",7));
       MCPList.insert(std::make_pair("Off4",8));
@@ -20,7 +20,7 @@ void Fill_inverted_MCPList (void)
       inverted_MCPList.insert(std::make_pair(2,"MiB2"));
       inverted_MCPList.insert(std::make_pair(3,"MiB3"));
       inverted_MCPList.insert(std::make_pair(4,"Planacon"));
-      inverted_MCPList.insert(std::make_pair(5,"Off1"));
+      inverted_MCPList.insert(std::make_pair(5,"Roma1"));
       inverted_MCPList.insert(std::make_pair(6,"Off2"));
       inverted_MCPList.insert(std::make_pair(7,"Off3"));
       inverted_MCPList.insert(std::make_pair(8,"Off4"));
