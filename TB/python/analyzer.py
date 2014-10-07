43bd2752d7182f87f9d6e59e91b5e7e126aa7029
Luca Brianza <brianza@hercules.mib.infn.it>
2014-10-07 14:24:08 +0200

    check treshold

--- TB/python/analyzer.py
+++ TB/python/analyzer.py
@@ -8,11 +8,11 @@ import argparse
 import string
 if __name__ == '__main__':
     parser = argparse.ArgumentParser (description = 'do iMCP TB analysis')
-    parser.add_argument ('-cfg', '--cfg' , default = 'cfg/treshold.txt' , help='cfg file containing treshold values')
+    parser.add_argument ('-cfg', '--cfg' , default = 'cfg/treshold_SB1000.txt' , help='cfg file containing treshold values')
     parser.add_argument ('-m', '--MCP' , default = 'MiB3', help='MCP to analyze')
     parser.add_argument ('-o', '--hodoCut' , default = 'no', help='hodoscope selection: no->no hodo cut, all->hit everywhere, tight->hit in the central 4 fibres')
     parser.add_argument ('-a', '--analysis' , default = "eff", help='select analysis: eff->efficiency, Q->charge, time=time resolution')
-    parser.add_argument ('-s', '--scan' , default = "HVScan", help='type of scan: HVScan=scan in HV, X0Scan = scan in X0')
+    parser.add_argument ('-s', '--scan' , default = "HV", help='type of scan: HVScan=scan in HV, X0Scan = scan in X0')
     parser.add_argument ('-n', '--name' , default = "Scan2", help='suffix of the reco file in input')
     args = parser.parse_args ()
     print 'analyzer '+args.cfg+' '+args.MCP+' '+args.hodoCut+' '+args.analysis+' '+args.scan+' '+args.name
