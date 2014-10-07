f40ba647eeec25cd0abf5e9f7f85bdf6fbe2f04e
Luca Brianza <brianza@hercules.mib.infn.it>
2014-10-07 18:10:32 +0200

    draw waveform

--- TB/compile.sh
+++ TB/compile.sh
@@ -1,3 +1,4 @@
 c++ -o calcTreshold bin/calcTreshold.cpp `root-config --cflags --glibs`;
 c++ -o analyzer bin/analyzer.cpp `root-config --cflags --glibs`;
 c++ -o dumper bin/dumper.cpp `root-config --cflags --glibs`;
+c++ -o drawWaveform bin/drawWaveform.cpp `root-config --cflags --glibs`
