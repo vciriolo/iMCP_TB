b7b0004ed2f691459bee8eb70e9f3ffe38d33a0b
lbrianza <lucabrianza@gmail.com>
2014-10-25 23:38:48 +0200

    update for H4 data

--- TB/python/dumper.py
+++ TB/python/dumper.py
@@ -8,10 +8,10 @@ import argparse
 import string
 if __name__ == '__main__':
     parser = argparse.ArgumentParser (description = 'dumper: create reco tree from raw data')
-    parser.add_argument ('-cfg', '--cfg' , default = 'cfg/Scan2.cfg' , help='list of run')
-    parser.add_argument ('-d', '--dir' , default = '/gwteray/users/marzocchi/iMCP/dataTrees', help='directory containing raw data')
+    parser.add_argument ('-cfg', '--cfg' , default = 'cfg/H4test.cfg' , help='list of run')
+    parser.add_argument ('-d', '--dir' , default = '/afs/cern.ch/user/l/lbrianza/work/public', help='directory containing raw data')
     parser.add_argument ('-c', '--chNumber' , default = '9', help='number of channels (in the raw data!)')
-    parser.add_argument ('-n', '--name' , default = 'Scan2', help='suffix of reco output file')
+    parser.add_argument ('-n', '--name' , default = 'H4test', help='suffix of reco output file')
     args = parser.parse_args ()
     print 'dumper '+args.cfg+' '+args.dir+' '+args.chNumber+' '+args.name
     os.system('./dumper '+args.cfg+' '+args.dir+' '+args.chNumber+' '+args.name)
