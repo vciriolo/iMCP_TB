96709f10747ee556af077fb2bc2dea65062493f4
lbrianza <lucabrianza@gmail.com>
2014-10-25 23:35:57 +0200

    fixed MCP map

--- TB/src/MCPMap.cc
+++ TB/src/MCPMap.cc
@@ -2,12 +2,12 @@
    
 void Fill_MCPList (void)
     {
-      MCPList.insert(std::make_pair("MiB1",0));
-      MCPList.insert(std::make_pair("Roma2",1));
-      MCPList.insert(std::make_pair("MiB2",2));
-      MCPList.insert(std::make_pair("MiB3",3));
-      MCPList.insert(std::make_pair("Planacon",4));
-      MCPList.insert(std::make_pair("Roma1",5));
+      MCPList.insert(std::make_pair("MiB2",0));
+      MCPList.insert(std::make_pair("ZStack1",1));
+      MCPList.insert(std::make_pair("EnSEE",2));
+      MCPList.insert(std::make_pair("ZStack2",3));
+      MCPList.insert(std::make_pair("MiB3",4));
+      MCPList.insert(std::make_pair("Off1",5));
       MCPList.insert(std::make_pair("Off2",6));
       MCPList.insert(std::make_pair("Off3",7));
       MCPList.insert(std::make_pair("Off4",8));
@@ -15,12 +15,12 @@ void Fill_MCPList (void)
 
 void Fill_inverted_MCPList (void)
     {
-      inverted_MCPList.insert(std::make_pair(0,"MiB1"));
-      inverted_MCPList.insert(std::make_pair(1,"Roma2"));
-      inverted_MCPList.insert(std::make_pair(2,"MiB2"));
-      inverted_MCPList.insert(std::make_pair(3,"MiB3"));
-      inverted_MCPList.insert(std::make_pair(4,"Planacon"));
-      inverted_MCPList.insert(std::make_pair(5,"Roma1"));
+      inverted_MCPList.insert(std::make_pair(0,"MiB2"));
+      inverted_MCPList.insert(std::make_pair(1,"ZStack1"));
+      inverted_MCPList.insert(std::make_pair(2,"EnSee"));
+      inverted_MCPList.insert(std::make_pair(3,"ZStack2"));
+      inverted_MCPList.insert(std::make_pair(4,"MiB3"));
+      inverted_MCPList.insert(std::make_pair(5,"Off1"));
       inverted_MCPList.insert(std::make_pair(6,"Off2"));
       inverted_MCPList.insert(std::make_pair(7,"Off3"));
       inverted_MCPList.insert(std::make_pair(8,"Off4"));
