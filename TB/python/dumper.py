9279f32395240ee8735fdd73981f80be69b614fe
lbrianza <lucabrianza@gmail.com>
2014-10-26 10:42:40 +0100

    update for H4 data

--- TB/python/dumper.py
+++ TB/python/dumper.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser (description = 'dumper: create reco tree from raw data')
     parser.add_argument ('-cfg', '--cfg' , default = 'cfg/H4test.cfg' , help='list of run')
     parser.add_argument ('-d', '--dir' , default = '/afs/cern.ch/user/l/lbrianza/work/public', help='directory containing raw data')
-    parser.add_argument ('-c', '--chNumber' , default = '9', help='number of channels (in the raw data!)')
+    parser.add_argument ('-c', '--chNumber' , default = '10', help='number of channels (in the raw data!)')
     parser.add_argument ('-n', '--name' , default = 'H4test', help='suffix of reco output file')
     args = parser.parse_args ()
     print 'dumper '+args.cfg+' '+args.dir+' '+args.chNumber+' '+args.name
