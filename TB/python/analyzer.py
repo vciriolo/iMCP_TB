8501f0d0ff3bc424842e86dc32e7972c54039e52
lbrianza <lucabrianza@gmail.com>
2014-10-27 18:30:56 +0100

    update

--- TB/python/analyzer.py
+++ TB/python/analyzer.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     parser.add_argument ('-o', '--hodoCut' , default = 'no', help='hodoscope selection: no->no hodo cut, all->hit everywhere, tight->hit in the central 4 fibres')
     parser.add_argument ('-a', '--analysis' , default = "eff", help='select analysis: eff->efficiency, Q->charge, time=time resolution')
     parser.add_argument ('-s', '--scan' , default = "HV", help='type of scan: HVScan=scan in HV, X0Scan = scan in X0')
-    parser.add_argument ('-n', '--name' , default = "H4test", help='suffix of the reco file in input')
+    parser.add_argument ('-n', '--name' , default = "HVScan1", help='suffix of the reco file in input')
     args = parser.parse_args ()
     print 'analyzer '+args.cfg+' '+args.MCP+' '+args.hodoCut+' '+args.analysis+' '+args.scan+' '+args.name
     os.system('./analyzer '+args.cfg+' '+args.MCP+' '+args.hodoCut+' '+args.analysis+' '+args.scan+' '+args.name)
