ebab6fa234d29ef75ab03c1aca9f8576dbce837d
lbrianza <lucabrianza@gmail.com>
2014-10-28 18:33:44 +0100

    removed hodo cut

--- TB/bin/analyzer.cpp
+++ TB/bin/analyzer.cpp
@@ -1,10 +1,10 @@
 /*****************************************************************************************
     This program compute charge or efficiency for all the run in a selected Scan for
-    selected MCP under test. Also the hodoscope cut are selectable.
+    selected MCP under test.
 
     compile with ---> c++ -o analyzer bin/analyzer.cpp `root-config --cflags --glibs --libs`
     run with:      ./analyzer cfg/treshold.txt MiB3 all eff HVScan Scan2
-          where the arguments are: 1) cfg file with the tresholds 2) MCP to analyze 3) type of hodoscope cut (all/tight) 4) analysis type (eff,time) 5) Scan type (HV or X0) 6) suffix of reco file in input
+          where the arguments are: 1) cfg file with the tresholds 2) MCP to analyze 4) analysis type (eff,time) 5) Scan type (HV or X0) 6) suffix of reco file in input
 
  ****************************************************************************************/
 #include <iostream>
@@ -39,17 +39,16 @@ using namespace std;
 int main(int argc, char** argv)
 {
   gSystem->Load("libTree");
-  char *label, *hodo_cut, *doWhat, *scanType;
+  char *label, *doWhat, *scanType;
   Fill_MCPList();
   Fill_inverted_MCPList();
 
   //read input options
   std::ifstream inputCfg (argv[1],ios::in);
   std::string MCP = argv[2];
-  hodo_cut = argv[3];
-  doWhat = argv[4];
-  scanType = argv[5];
-  label = argv[6];
+  doWhat = argv[3];
+  scanType = argv[4];
+  label = argv[5];
 
   std::cout<<"----START ANALYZER: analyzing MCP "<<MCP<<", mode: "<<scanType<<", scan: "<<label<<"-------"<<std::endl;
  
@@ -74,22 +73,17 @@ int main(int argc, char** argv)
   InitRecoTree(nt);
   
   char outputFileName[200]="";
-  sprintf(outputFileName, "results/%s_%s_%s_%s_%s.txt", MCP.c_str(), hodo_cut, doWhat, scanType, label);
+  sprintf(outputFileName, "results/%s_%s_%s_%s.txt", MCP.c_str(), doWhat, scanType, label);
   std::ofstream outputFile (outputFileName, std::ofstream::out);
 
   //------Build TCut and draw variables--------------------------------------------------
   char str_cut_sig[200]="";
-  char str_cut_sig_2D[200]="";
   char str_cut_trig0[200]="";
-  char str_cut_trig1[200]="";
-  char str_cut_hodoX[250]="";
-  char str_cut_hodoY[250]="";
   char str_cut_acc[250]="";
   char var_sig[100]="";
   char var_base[100]="";
   char var_time[100]="";
   char var_trig0[100]="";
-  char var_trig1[100]="";
   char var_acc[100]="";
 
   //---open tree and get: run list and corresponding HV/X0-----
@@ -99,6 +93,7 @@ int main(int argc, char** argv)
   X0Step.clear();
   std::vector<float> ScanList;
   ScanList.clear();
+
 
   //---save list of HV (or X0) step---
   if (strcmp(scanType,"HV")==0) {
@@ -153,36 +148,12 @@ int main(int argc, char** argv)
 
   //cut strings
   sprintf(str_cut_sig, "charge[%d] > %d", MCPNumber, treshold.at(MCPNumber));
-	//sprintf(str_cut_sig_2D, "-charge_%s > -13.28*amp_max_%s - 350", MCP, MCP);
   sprintf(str_cut_trig0, "charge[%d] > %d", trigPos1, treshold.at(trigPos1));
   sprintf(str_cut_acc, "charge[%d] > %d && charge[%d] < %d", MCPNumber, treshold.at(MCPNumber), trigPos1, treshold.at(trigPos1));
 
-  //  sprintf(str_cut_trig0, "charge[%d] < %d  && charge[%d] < %d && sci_front_adc < 500", trigPos1, treshold.at(trigPos1), trigPos2, treshold.at(trigPos2));
-  //  sprintf(str_cut_trig1, "charge[%d] > %d  && charge[%d] > %d && sci_front_adc > 500 && sci_front_adc < 1500",trigPos1, treshold.at(trigPos1), trigPos2, treshold.at(trigPos2));
-
-  //---Hodoscope cut
-  float thX[8]={113,95,127,118,94,134,133,160};
-  float thY[8]={110,101,119,136,94,98,87,97};
-  if(strcmp(hodo_cut,"all") == 0)
-    {
-	sprintf(str_cut_hodoX, "(fibreX[0] > %f || fibreX[1] > %f || fibreX[2] > %f || fibreX[3] > %f || fibreX[4] > %f || "
-		"fibreX[5] > %f || fibreX[6] > %f || fibreX[7] > %f)", thX[0], thX[1], thX[2], thX[3], thX[4], thX[5], thX[6], thX[7]);
-	sprintf(str_cut_hodoY, "(fibreY[0] > %f || fibreY[1] > %f || fibreY[2] > %f || fibreY[3] > %f || fibreY[4] > %f || "
-		"fibreY[5] > %f || fibreY[6] > %f || fibreY[7] > %f)", thY[0], thY[1], thY[2], thY[3], thY[4], thY[5], thY[6], thY[7]);
-    }
-  else if(strcmp(hodo_cut,"tight") == 0)
-    {
-	sprintf(str_cut_hodoX, "(fibreX[3] > %f || fibreX[4] > %f)", thX[3], thX[4]);
-	sprintf(str_cut_hodoY, "(fibreY[3] > %f || fibreY[4] > %f)", thY[3], thY[4]);
-    }
-
   //-----construct TCut-----
   TCut cut_sig = str_cut_sig;
-  TCut cut_sig_2D = str_cut_sig_2D;
   TCut cut_trig0 = str_cut_trig0;
-  TCut cut_trig1 = str_cut_trig1;
-  TCut cut_hodoX = str_cut_hodoX;
-  TCut cut_hodoY = str_cut_hodoY;
   TCut cut_acc = str_cut_acc;
 
   TGraphErrors *g_eff = new TGraphErrors(ScanList.size());
@@ -192,10 +163,9 @@ int main(int argc, char** argv)
   for(unsigned int i=0; i<ScanList.size(); i++)
     {
 
-      char h_sig_name[20], h_base_name[20], h_trig1_name[20], h_trig0_name[20], res_func_name[20], h_time_name[20], h_acc_name[20];
+      char h_sig_name[20], h_base_name[20], h_trig0_name[20], res_func_name[20], h_time_name[20], h_acc_name[20];
       sprintf(h_sig_name, "h_sig_%d", i);
       sprintf(h_base_name, "h_base_%d", i);
-      sprintf(h_trig1_name, "h_trig1_%d", i);
       sprintf(h_trig0_name, "h_trig0_%d", i);
       sprintf(h_time_name, "h_time_%d", i);
       sprintf(res_func_name, "res_func_%d", i);
@@ -203,7 +173,6 @@ int main(int argc, char** argv)
 
       TH1F* h_sig= new TH1F(h_sig_name, h_sig_name, 500, -5000, 25000);
       TH1F* h_base = new TH1F(h_base_name, h_base_name, 500, 5000, 25000);
-      TH1F* h_trig1 = new TH1F(h_trig1_name, h_trig1_name, 500, -5000, 25000);
       TH1F* h_trig0 = new TH1F(h_trig0_name, h_trig0_name, 500, -5000, 25000);
       TH1F* h_time = new TH1F(h_time_name, h_time_name, 500, -5, 5);
       TH1F* h_acc= new TH1F(h_acc_name, h_acc_name, 500, -5000, 25000);
@@ -214,7 +183,6 @@ int main(int argc, char** argv)
       sprintf(var_base, "baseline[%d]>>%s", MCPNumber, h_base_name);
       sprintf(var_time, "(time_CF[%d]-time_CF[%d])>>%s", MCPNumber, trigPos1, h_time_name);
       sprintf(var_trig0, "charge[%d]>>%s", trigPos1, h_trig0_name);
-      sprintf(var_trig1, "charge[%d]>>%s", trigPos1, h_trig1_name);
       sprintf(var_acc, "charge[%d]>>%s", MCPNumber, h_acc_name);
 
       char cut_scan[100];
@@ -225,14 +193,11 @@ int main(int argc, char** argv)
       nt->Draw(var_sig, cut_trig0 && cut_sig && cut_scan, "goff");
       nt->Draw(var_base, cut_trig0 && cut_sig && cut_scan, "goff");
       nt->Draw(var_trig0,cut_trig0 && cut_scan, "goff");
-      nt->Draw(var_trig1,cut_trig1 && cut_scan, "goff");
       nt->Draw(var_acc, cut_acc && cut_scan, "goff");
-     
-
+    
       //            std::cout<<h_sig->GetEntries()<<" "<<h_acc->GetEntries()<<" "<<h_trig0->GetEntries()<<std::endl;
-      
-            float eff = ((h_sig->GetEntries()-h_acc->GetEntries())/h_trig0->GetEntries());
-            float e_eff = TMath::Sqrt((TMath::Abs(eff*(1-eff)))/h_trig0->GetEntries());
+      float eff = ((h_sig->GetEntries()-h_acc->GetEntries())/h_trig0->GetEntries());
+      float e_eff = TMath::Sqrt((TMath::Abs(eff*(1-eff)))/h_trig0->GetEntries());
 
 	    //      float eff = (h_sig->GetEntries()-h_base->GetEntries()*h_trig1->GetEntries()/h_trig0->GetEntries())/h_trig1->GetEntries();
 	    //     float e_eff = TMath::Sqrt((TMath::Abs(eff*(1-eff)))/h_trig1->GetEntries());
@@ -272,7 +237,7 @@ int main(int argc, char** argv)
 		printf(" %s\tQ\te_%s\te_Q\n", var_name, var_name);
 		printf("-----------------------------\n");
 	    }
-	    nt->Draw(var_sig, cut_trig1 && cut_sig_2D && cut_hodoX && cut_hodoY && cut_scan);
+	    nt->Draw(var_sig, cut_scan);
 	    if(TString(scanType).Contains("HV") == 1) {
 	      printf("%d\t%.0f\t%.0f\t%.0f\n", HVVal.at(i), h_sig->GetMean(), 0., h_sig->GetMeanError());
 	      outputFile << HVVal.at(i)<<"\t"<<h_sig->GetMean()<<"\t 0.\t"<<h_sig->GetMeanError()<<std::endl;
@@ -350,11 +315,11 @@ int main(int argc, char** argv)
       g_eff->GetXaxis()->SetRangeUser(1100,3500);
       g_eff->Draw("AP");
       char effPlotName[200]="";
-      sprintf(effPlotName, "plots/efficiency/efficiency_%s_%s_%s_%s_%s.pdf", MCP.c_str(), hodo_cut, doWhat, scanType, label);
+      sprintf(effPlotName, "plots/efficiency/efficiency_%s_%s_%s_%s.pdf", MCP.c_str(), doWhat, scanType, label);
       c2->Print(effPlotName,"pdf");
 
             char effRootName[200]="";
-            sprintf(effRootName, "plots/efficiency/efficiency_%s_%s_%s_%s_%s.root", MCP.c_str(), hodo_cut, doWhat, scanType, label);
+            sprintf(effRootName, "plots/efficiency/efficiency_%s_%s_%s_%s.root", MCP.c_str(), doWhat, scanType, label);
 
 	    TFile *out = new TFile (TString(effRootName),"RECREATE");
       out->cd();
