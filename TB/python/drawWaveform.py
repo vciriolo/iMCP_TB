09155aaf766ea09b3e3e86ae9c873ac5868f2232
arabella <arabella.martelli@cern.ch>
2014-10-29 10:18:40 +0100

    draw

--- TB/python/drawWaveform.py
+++ TB/python/drawWaveform.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser (description = 'draw waveform: read raw data and draw some waveforms')
     parser.add_argument ('-d', '--dir' , default = '/data/public_DQM_plots', help='directory containing raw data')
     parser.add_argument ('-r', '--run' , default = '744', help='run to scan')
-    parser.add_argument ('-c', '--channel' , default = '0', help='channel to analyze')
+    parser.add_argument ('-c', '--channel' , default = '4', help='channel to analyze')
     parser.add_argument ('-f', '--firstEntry' , default = '0', help='first entry')
     parser.add_argument ('-n', '--nEvents' , default = '100', help='number of events to analyze')
     args = parser.parse_args ()
