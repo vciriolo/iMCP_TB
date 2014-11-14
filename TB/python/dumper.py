c1eaa5f0f032b80fd54cfccab40c9c69cff2c32d
Luca Brianza <brianza@hercules.mib.infn.it>
2014-11-14 18:08:54 +0100

    hercules folder

--- TB/python/dumper.py
+++ TB/python/dumper.py
@@ -9,7 +9,7 @@ import string
 if __name__ == '__main__':
     parser = argparse.ArgumentParser (description = 'dumper: create reco tree from raw data')
     parser.add_argument ('-cfg', '--cfg' , default = 'cfg/HVScan1.cfg' , help='list of run')
-    parser.add_argument ('-d', '--dir' , default = '/data_secure/public_DQM_plots_secure', help='directory containing raw data')
+    parser.add_argument ('-d', '--dir' , default = '/gwteray/users/brianza/iMCP_H4', help='directory containing raw data')
     parser.add_argument ('-c', '--chNumber' , default = '10', help='number of channels (in the raw data!)')
     parser.add_argument ('-n', '--name' , default = 'HVScan1', help='suffix of reco output file')
     args = parser.parse_args ()
