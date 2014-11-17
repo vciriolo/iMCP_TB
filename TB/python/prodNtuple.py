736e50aef56382ef5e18cba1a2103805db557ee9
Luca Brianza <brianza@hercules.mib.infn.it>
2014-11-17 16:33:38 +0100

    update for batch submission

--- TB/python/prodNtuple.py
+++ TB/python/prodNtuple.py
@@ -10,18 +10,18 @@ import subprocess
 currentDir = os.getcwd();
 CMSSWDir = currentDir+"../../";
 
-name = ["HVScan1","HVScan2","HVScan3","HVScan4","HVScan5","HVScan6","HVScan7","HVScan8"];
+name = ["HVScan1","HVScan2","HVScan3","HVScan4","HVScan5","HVScan6","HVScan7","HVScan8","X0scan_HVH_20GeV","X0scan_HVH_50GeV","X0scan_HVL_20GeV","X0scan_HVL_50GeV","AngScan_HVHigh","AngScan_HVLow","pedestal","preshower_600GeV_HighV"];
 
 for i in range(len(name)):
     fn = "Job/Job_"+name[i];
     outScript = open(fn+".sh","w");
-    command = "python python/dumper.py -cfg cfg/"+name[i]+".cfg -n "+name[i];
+    command = "python python/dumper_fitTemplate.py -cfg cfg/"+name[i]+".cfg -n "+name[i];
 
     outScript.write('#!/bin/bash');
     outScript.write("\n"+'cd '+CMSSWDir);
     outScript.write("\n"+'eval `scram runtime -sh`');
     outScript.write("\n"+'cd '+currentDir);
-    outScript.write("\n"+"unbuffer "+command+" > "+fn+".txt");
+    outScript.write("\n"+"unbuffer "+command+" > "+fn+"_output.txt");
     outScript.close();
 
     os.system("chmod 777 "+currentDir+"/"+fn+".sh");
