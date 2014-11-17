79a70d3a74b50b56c8dc9113c4df2f89668c654b
Luca Brianza <brianza@hercules.mib.infn.it>
2014-11-17 14:52:57 +0100

    update

--- TB/python/prodNtuple.py
+++ TB/python/prodNtuple.py
@@ -8,20 +8,20 @@ import time
 import subprocess
 
 currentDir = os.getcwd();
-CMSSWDir = "/gwpool/users/brianza/PHD/CMSSW_6_1_1/src/";
+CMSSWDir = currentDir+"../../";
 
 name = ["HVScan1","HVScan2","HVScan3","HVScan4","HVScan5","HVScan6","HVScan7","HVScan8"];
 
 for i in range(len(name)):
-    fn = "Job_"+name[i];
+    fn = "Job/Job_"+name[i];
     outScript = open(fn+".sh","w");
-    command = "python python/dumper.py -cfg cfg/treshold_5s.txt -n "+name[i];
+    command = "python python/dumper.py -cfg cfg/"+name[i]+".cfg -n "+name[i];
 
     outScript.write('#!/bin/bash');
     outScript.write("\n"+'cd '+CMSSWDir);
     outScript.write("\n"+'eval `scram runtime -sh`');
     outScript.write("\n"+'cd '+currentDir);
-    outScript.write("\n"+"unbuffer "+command+" > /gwteray/users/brianza/output_iMCP/"+fn+".txt");
+    outScript.write("\n"+"unbuffer "+command+" > Job/"+fn+".txt");
     outScript.close();
 
     os.system("chmod 777 "+currentDir+"/"+fn+".sh");
