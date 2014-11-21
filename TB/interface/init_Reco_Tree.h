933d7adb30146e6a894a44ed4da2a93fc296d011
Luca Brianza <brianza@hercules.mib.infn.it>
2014-11-21 14:51:59 +0100

    save time start, time stop

--- TB/interface/init_Reco_Tree.h
+++ TB/interface/init_Reco_Tree.h
@@ -8,9 +8,12 @@
 extern float                 time_CF[10];
 extern float                 time_CF_corr[10];
 extern float                 time_OT[10];
+extern float                 time_start[10];
+extern float                 time_stop[10];
 extern float                 time_Max[10];
 extern float                 amp_max[10];
 extern float                 amp_max_corr[10];
+extern float                 amp_max_time[10];
 extern float                 charge[10];
 extern float                 charge_corr[10];
 extern float                 baseline[10];
@@ -28,10 +31,13 @@ extern float tdcY;
 // List of branches                                                                                                                                               
 extern TBranch        *b_time_CF;   //!                                            
 extern TBranch        *b_time_CF_corr;   //!                                   
-extern TBranch        *b_time_OT;   //!                            
+extern TBranch        *b_time_OT;   //!     
+extern TBranch        *b_time_start;                       
+extern TBranch        *b_time_stop;                       
 extern TBranch        *b_time_Max;   //!                                   
 extern TBranch        *b_amp_max;   //!                                        
 extern TBranch        *b_amp_max_corr;   //!                                                          
+extern TBranch        *b_amp_max_time;   //!                                                          
 extern TBranch        *b_baseline;   //!                                      
 extern TBranch        *b_charge;   //!                                     
 extern TBranch        *b_charge_corr;   //!                              
