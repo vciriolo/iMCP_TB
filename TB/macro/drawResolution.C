5717fdc6b12b30639316cf900e4c125885325b30
amartelli <arabella.martelli@cern.ch>
2014-11-24 12:30:57 +0100

    plot timing

--- TB/macro/drawResolution.C
+++ TB/macro/drawResolution.C
@@ -1,9 +1,11 @@
 {
 
   //    std::string HVE = "EffAll";
-      std::string HVE = "AngScanLow";
+  //      std::string HVE = "AngScanLow";
   //  std::string HVE = "HVL_50";
   //  std::string HVE = "HVL_20";
+  //  std::string HVE = "HVH_20";
+  std::string HVE = "HVH_50";
 
   TFile* inF_MiB3_ON;
   TFile* inF_MiB3_OFF;
@@ -15,24 +17,33 @@
 
   TMultiGraph *mg = new TMultiGraph();
 
+  TLegend *legC = new TLegend(0.50,0.20,0.79,0.4,NULL,"brNDC");
+  legC->SetTextFont(42);
+  legC->SetTextSize(0.03);
+  legC->SetFillColor(kWhite);
+  legC->SetLineColor(kWhite);
+  legC->SetShadowColor(kWhite);
+
+  TCanvas* c_HVH_50 = new TCanvas();
+
   if(HVE == "EffAll"){
-    inF_MiB3_ON = TFile::Open("plots/time_resolution/time_MiB3_eff_HV_HVScan2.root");
-    inF_MiB3_OFF = TFile::Open("plots/time_resolution/time_MiB3_eff_HV_HVScan7.root");
-    inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_eff_HV_HVScan5.root");
-    inF_ZStack1_ON = TFile::Open("plots/time_resolution/time_ZStack1_eff_HV_HVScan1.root");
-    inF_ZStack1_OFF = TFile::Open("plots/time_resolution/time_ZStack1_eff_HV_HVScan8.root");
-    inF_ZStack2_ON = TFile::Open("plots/time_resolution/time_ZStack2_eff_HV_HVScan2.root");
-    inF_ZStack2_OFF = TFile::Open("plots/time_resolution/time_ZStack2_eff_HV_HVScan6.root");
+    inF_MiB3_ON = TFile::Open("plots/time_resolution/time_MiB3_time_HV_HVScan2.root");
+    inF_MiB3_OFF = TFile::Open("plots/time_resolution/time_MiB3_time_HV_HVScan7.root");
+    inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_time_HV_HVScan5.root");
+    inF_ZStack1_ON = TFile::Open("plots/time_resolution/time_ZStack1_time_HV_HVScan1.root");
+    inF_ZStack1_OFF = TFile::Open("plots/time_resolution/time_ZStack1_time_HV_HVScan8.root");
+    inF_ZStack2_ON = TFile::Open("plots/time_resolution/time_ZStack2_time_HV_HVScan2.root");
+    inF_ZStack2_OFF = TFile::Open("plots/time_resolution/time_ZStack2_time_HV_HVScan6.root");
 
     TGraphErrors* eff_MiB3_ON = (TGraphErrors*)inF_MiB3_ON->Get("eff");
-  TGraphErrors* eff_MiB3_OFF = (TGraphErrors*)inF_MiB3_OFF->Get("eff");
+    TGraphErrors* eff_MiB3_OFF = (TGraphErrors*)inF_MiB3_OFF->Get("eff");
     TGraphErrors* eff_enSEE = (TGraphErrors*)inF_enSEE->Get("eff");
-     TGraphErrors* eff_ZStack2_ON = (TGraphErrors*)inF_ZStack2_ON->Get("eff");
-     TGraphErrors* eff_ZStack1_ON = (TGraphErrors*)inF_ZStack1_ON->Get("eff");
-     //     TGraphErrors* eff_ZStack2_OFF = (TGraphErrors*)inF_ZStack2_OFF->Get("eff");
-  TGraphErrors* eff_ZStack1_OFF = (TGraphErrors*)inF_ZStack1_OFF->Get("eff");
-  TGraphErrors* eff_ZStack2_OFF = (TGraphErrors*)inF_ZStack2_OFF->Get("eff");
-
+    TGraphErrors* eff_ZStack2_ON = (TGraphErrors*)inF_ZStack2_ON->Get("eff");
+    TGraphErrors* eff_ZStack1_ON = (TGraphErrors*)inF_ZStack1_ON->Get("eff");
+    //     TGraphErrors* eff_ZStack2_OFF = (TGraphErrors*)inF_ZStack2_OFF->Get("eff");
+    TGraphErrors* eff_ZStack1_OFF = (TGraphErrors*)inF_ZStack1_OFF->Get("eff");
+    TGraphErrors* eff_ZStack2_OFF = (TGraphErrors*)inF_ZStack2_OFF->Get("eff");
+  
 
   //settings
   eff_MiB3_ON->SetMarkerColor(kGreen+1);
@@ -66,12 +77,6 @@
   eff_enSEE->SetLineWidth(2);
 
 
-  TLegend *legC = new TLegend(0.50,0.20,0.79,0.4,NULL,"brNDC");
-  legC->SetTextFont(42);
-  legC->SetTextSize(0.03);
-  legC->SetFillColor(kWhite);
-  legC->SetLineColor(kWhite);
-  legC->SetShadowColor(kWhite);
 
   legC->AddEntry(eff_MiB3_ON, "MiB3 chevron-ON", "p");
   legC->AddEntry(eff_MiB3_OFF, "MiB3 chevron-OFF", "p");
@@ -81,25 +86,24 @@
   legC->AddEntry(eff_ZStack1_OFF, "ZStack1 OFF", "p");
   legC->AddEntry(eff_enSEE, "SEE OFF", "p");
 
-  TCanvas* c_HVH_50 = new TCanvas();
   gPad->SetTicks();
   if(HVE == "HVH_50" || "HVL_50" || "EffAll")  eff_enSEE->SetTitle("50 GeV");
   if(HVE == "HVH_20" || "HVL_20")  eff_enSEE->SetTitle("50 GeV");
 
-    eff_MiB3_OFF->GetXaxis()->SetTitle("Angle (degrees)");
+  eff_MiB3_OFF->GetXaxis()->SetTitle("Angle (degrees)");
   eff_MiB3_OFF->GetYaxis()->SetTitle("Time_Res (Ps)");
   eff_enSEE->GetXaxis()->SetTitle("Angle (degrees)");
   eff_enSEE->GetYaxis()->SetTitle("Time_Res (Ps)");
   eff_ZStack1_OFF->GetXaxis()->SetTitle("Angle (degrees)");
   eff_ZStack1_OFF->GetYaxis()->SetTitle("Time_Res (Ps)");
   
-    mg->Add(eff_MiB3_ON);
+  mg->Add(eff_MiB3_ON);
   mg->Add(eff_MiB3_OFF);
-    mg->Add(eff_ZStack1_ON);
+  mg->Add(eff_ZStack1_ON);
   mg->Add(eff_ZStack1_OFF);
   mg->Add(eff_ZStack2_OFF);
-   mg->Add(eff_ZStack2_ON);
-   mg->Add(eff_ZStack2_OFF);
+  mg->Add(eff_ZStack2_ON);
+  mg->Add(eff_ZStack2_OFF);
   mg->Add(eff_enSEE);
 
   mg->Draw("APL");
@@ -108,44 +112,43 @@
   mg->SetMinimum(0);
   mg->Draw("APL");  
   legC->Draw("same");
-
   }
 
   else {
   if(HVE == "AngScanHigh"){
-  inF_MiB3 = TFile::Open("plots/time_resolution/time_MiB3_eff_X0_AngScan_HVHigh.root");
-  inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_eff_X0_AngScan_HVHigh.root");
-  inF_ZStack1 = TFile::Open("plots/time_resolution/time_ZStack1_eff_X0_AngScan_HVHigh.root");
+  inF_MiB3 = TFile::Open("plots/time_resolution/time_MiB3_time_X0_AngScan_HVHigh.root");
+  inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_time_X0_AngScan_HVHigh.root");
+  inF_ZStack1 = TFile::Open("plots/time_resolution/time_ZStack1_time_X0_AngScan_HVHigh.root");
   }
   if(HVE == "AngScanLow"){
-  inF_MiB3 = TFile::Open("plots/time_resolution/time_MiB3_eff_X0_AngScan_HVLow.root");
-  inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_eff_X0_AngScan_HVLow.root");
-  inF_ZStack1 = TFile::Open("plots/time_resolution/time_ZStack1_eff_X0_AngScan_HVLow.root");
+  inF_MiB3 = TFile::Open("plots/time_resolution/time_MiB3_time_X0_AngScan_HVLow.root");
+  inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_time_X0_AngScan_HVLow.root");
+  inF_ZStack1 = TFile::Open("plots/time_resolution/time_ZStack1_time_X0_AngScan_HVLow.root");
   }
 
   if(HVE == "HVH_50"){
-  inF_MiB3 = TFile::Open("plots/time_resolution/time_MiB3_eff_X0_scanX0_HVHigh50GeV.root");
-  inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_eff_X0_scanX0_HVHigh50GeV.root");
-  inF_ZStack2 = TFile::Open("plots/time_resolution/time_ZStack2_eff_X0_scanX0_HVHigh50GeV.root");
-  inF_ZStack1 = TFile::Open("plots/time_resolution/time_ZStack1_eff_X0_scanX0_HVHigh50GeV.root");
+  inF_MiB3 = TFile::Open("plots/time_resolution/time_MiB3_time_X0_scanX0_HVHigh50GeV.root");
+  inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_time_X0_scanX0_HVHigh50GeV.root");
+  inF_ZStack2 = TFile::Open("plots/time_resolution/time_ZStack2_time_X0_scanX0_HVHigh50GeV.root");
+  inF_ZStack1 = TFile::Open("plots/time_resolution/time_ZStack1_time_X0_scanX0_HVHigh50GeV.root");
   }
   if(HVE == "HVH_20"){
-  inF_MiB3 = TFile::Open("plots/time_resolution/time_MiB3_eff_X0_scanX0_HVHigh20GeV.root");
-  inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_eff_X0_scanX0_HVHigh20GeV.root");
-  inF_ZStack2 = TFile::Open("plots/time_resolution/time_ZStack2_eff_X0_scanX0_HVHigh20GeV.root");
-  inF_ZStack1 = TFile::Open("plots/time_resolution/time_ZStack1_eff_X0_scanX0_HVHigh20GeV.root");
+  inF_MiB3 = TFile::Open("plots/time_resolution/time_MiB3_time_X0_scanX0_HVHigh20GeV.root");
+  inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_time_X0_scanX0_HVHigh20GeV.root");
+  inF_ZStack2 = TFile::Open("plots/time_resolution/time_ZStack2_time_X0_scanX0_HVHigh20GeV.root");
+  inF_ZStack1 = TFile::Open("plots/time_resolution/time_ZStack1_time_X0_scanX0_HVHigh20GeV.root");
   }
   if(HVE == "HVL_50"){
-  inF_MiB3 = TFile::Open("plots/time_resolution/time_MiB3_eff_X0_scanX0_HVLow50GeV.root");
-  inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_eff_X0_scanX0_HVLow50GeV.root");
-  inF_ZStack2 = TFile::Open("plots/time_resolution/time_ZStack2_eff_X0_scanX0_HVLow50GeV.root");
-  inF_ZStack1 = TFile::Open("plots/time_resolution/time_ZStack1_eff_X0_scanX0_HVLow50GeV.root");
+  inF_MiB3 = TFile::Open("plots/time_resolution/time_MiB3_time_X0_scanX0_HVLow50GeV.root");
+  inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_time_X0_scanX0_HVLow50GeV.root");
+  inF_ZStack2 = TFile::Open("plots/time_resolution/time_ZStack2_time_X0_scanX0_HVLow50GeV.root");
+  inF_ZStack1 = TFile::Open("plots/time_resolution/time_ZStack1_time_X0_scanX0_HVLow50GeV.root");
   }
   if(HVE == "HVL_20"){
-  inF_MiB3 = TFile::Open("plots/time_resolution/time_MiB3_eff_X0_scanX0_HVLow20GeV.root");
-  inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_eff_X0_scanX0_HVLow20GeV.root");
-  inF_ZStack2 = TFile::Open("plots/time_resolution/time_ZStack2_eff_X0_scanX0_HVLow20GeV.root");
-  inF_ZStack1 = TFile::Open("plots/time_resolution/time_ZStack1_eff_X0_scanX0_HVLow20GeV.root");
+  inF_MiB3 = TFile::Open("plots/time_resolution/time_MiB3_time_X0_scanX0_HVLow20GeV.root");
+  inF_enSEE = TFile::Open("plots/time_resolution/time_enSEE_time_X0_scanX0_HVLow20GeV.root");
+  inF_ZStack2 = TFile::Open("plots/time_resolution/time_ZStack2_time_X0_scanX0_HVLow20GeV.root");
+  inF_ZStack1 = TFile::Open("plots/time_resolution/time_ZStack1_time_X0_scanX0_HVLow20GeV.root");
   }
 
 
@@ -179,14 +182,14 @@
   eff_enSEE->SetMarkerStyle(20);
   eff_enSEE->SetLineWidth(2);
 
-
+  /*
   TLegend *legC = new TLegend(0.50,0.20,0.79,0.4,NULL,"brNDC");
   legC->SetTextFont(42);
   legC->SetTextSize(0.03);
   legC->SetFillColor(kWhite);
   legC->SetLineColor(kWhite);
   legC->SetShadowColor(kWhite);
-
+  */
 
   if(HVE == "AngScanLow"){
   legC->AddEntry(eff_MiB3, "MiB3 chevron-OFF HV 3000", "p");
@@ -214,7 +217,7 @@
   legC->AddEntry(eff_enSEE, "SEE OFF  HV 1900", "p");
   }
 
-  TCanvas* c_HVH_50 = new TCanvas();
+  //  TCanvas* c_HVH_50 = new TCanvas();
   gPad->SetTicks();
   
   mg->Add(eff_MiB3);
@@ -230,9 +233,7 @@
   mg->SetMinimum(0);
   mg->Draw("APL");
 
-  
   legC->Draw("same");
-
   //  c_HVH_50->Print("plots/X0scan/");
   }
 }
