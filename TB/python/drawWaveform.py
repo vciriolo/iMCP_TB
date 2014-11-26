55f54eed2f3e79fac35f3887d8447050c0c8c9f6
Luca Brianza <brianza@hercules.mib.infn.it>
2014-11-26 16:41:44 +0100

    update directory for hercules

--- TB/python/drawWaveform.py
+++ TB/python/drawWaveform.py
@@ -8,7 +8,7 @@ import argparse
 import string
 if __name__ == '__main__':
     parser = argparse.ArgumentParser (description = 'draw waveform: read raw data and draw some waveforms')
-    parser.add_argument ('-d', '--dir' , default = '/data_secure/public_DQM_plots_secure', help='directory containing raw data')
+    parser.add_argument ('-d', '--dir' , default = '/gwteray/users/brianza/iMCP_H4', help='directory containing raw data')
     parser.add_argument ('-r', '--run' , default = '744', help='run to scan')
     parser.add_argument ('-c', '--channel' , default = '4', help='channel to analyze')
     parser.add_argument ('-f', '--firstEntry' , default = '0', help='first entry')
