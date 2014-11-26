678cf0944899cd46ee871a66e248b70d8e527c38
Luca Brianza <brianza@hercules.mib.infn.it>
2014-11-26 17:06:50 +0100

    changed default treshold file (5sigma) for analyzer

--- TB/python/analyzer.py
+++ TB/python/analyzer.py
@@ -8,7 +8,7 @@ import argparse
 import string
 if __name__ == '__main__':
     parser = argparse.ArgumentParser (description = 'do iMCP TB analysis')
-    parser.add_argument ('-cfg', '--cfg' , default = 'cfg/treshold_SB1000.txt' , help='cfg file containing treshold values')
+    parser.add_argument ('-cfg', '--cfg' , default = 'cfg/treshold_5s.txt' , help='cfg file containing treshold values')
     parser.add_argument ('-m', '--MCP' , default = 'MiB3', help='MCP to analyze')
     parser.add_argument ('-a', '--analysis' , default = "eff", help='select analysis: eff->efficiency, Q->charge, time=time resolution')
     parser.add_argument ('-s', '--scan' , default = "HV", help='type of scan: HVScan=scan in HV, X0Scan = scan in X0')
