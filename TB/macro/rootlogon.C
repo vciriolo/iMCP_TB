398b06cacfbb730f003eacd2f2ca003d3b8e205e
Luca Brianza <brianza@hercules.mib.infn.it>
2014-12-01 10:27:23 +0100

    merge pull request from lpernie- hodoscope

---
+++ TB/macro/rootlogon.C
@@ -0,0 +1,6 @@
+// This is the file rootlogon.C
+{
+  //gROOT->ProcessLine(".include /afs/cern.ch/cms/slc6_amd64_gcc472/lcg/roofit/5.34.04-cms5/include/");
+  //gROOT->ProcessLine(".include /afs/cern.ch/cms/slc5_amd64_gcc472/lcg/roofit/5.34.07-cms3/include/");
+  gROOT->ProcessLine(".include /afs/cern.ch/cms/slc6_amd64_gcc481/lcg/roofit/5.34.22/include/");
+}
