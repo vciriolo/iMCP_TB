eca99cae3493edf89e6d8d3ec425a5bd65ba69d9
Luca Brianza <brianza@hercules.mib.infn.it>
2014-12-08 10:39:00 +0100

    added number of hodoscope fibers ON

--- TB/interface/init_Reco_Tree.h
+++ TB/interface/init_Reco_Tree.h
@@ -5,6 +5,7 @@
 #include "TChain.h"
 
 // Declaration of leaf types 
+extern int                 event;
 extern float                 time_CF[10];
 extern float                 time_CF_corr[10];
 extern float                 time_OT[10];
@@ -27,9 +28,6 @@ extern float                 amp_max_time[10];
 extern float                 charge[10];
 extern float                 charge_corr[10];
 extern float                 baseline[10];
-extern int                   fibreX[8];
-extern int                   fibreY[8];
-extern int                   sci_front_adc;
 extern int                   run_id;
 extern int                   isPCOn[10];
 extern int                   HV[10];
@@ -41,8 +39,18 @@ extern bool hodoX1[64];
 extern bool hodoY1[64];
 extern bool hodoX2[64];
 extern bool hodoY2[64];
+extern int nhodoX1;
+extern int nhodoX2;
+extern int nhodoY1;
+extern int nhodoY2;
+/*
+extern int                   fibreX[8];
+extern int                   fibreY[8];
+extern int                   sci_front_adc;
+*/
 
 // List of branches                 
+extern TBranch        *b_event;
 extern TBranch        *b_time_CF;   //!                                            
 extern TBranch        *b_time_CF_corr;   //!                                   
 extern TBranch        *b_time_OT;   //!     
@@ -65,9 +73,6 @@ extern TBranch        *b_amp_max_time;   //!
 extern TBranch        *b_baseline;   //!                                      
 extern TBranch        *b_charge;   //!                                     
 extern TBranch        *b_charge_corr;   //!                              
-extern TBranch        *b_fibreX;   //!                                    
-extern TBranch        *b_fibreY;   //!                                        
-extern TBranch        *b_sci_front_adc;   //!                               
 extern TBranch        *b_run_id;   //!                              
 extern TBranch        *b_isPCOn;   //!  
 extern TBranch        *b_HV;   //!     
@@ -79,6 +84,15 @@ extern TBranch *b_hodoX1;
 extern TBranch *b_hodoY1;
 extern TBranch *b_hodoX2;
 extern TBranch *b_hodoY2;
+extern TBranch *b_nhodoX1;
+extern TBranch *b_nhodoY1;
+extern TBranch *b_nhodoX2;
+extern TBranch *b_nhodoY2;
+/*
+extern TBranch        *b_fibreX;   //!                                    
+extern TBranch        *b_fibreY;   //!                                        
+extern TBranch        *b_sci_front_adc;   //!                               
+*/
 
 void InitRecoTree(TTree* nt);
 
