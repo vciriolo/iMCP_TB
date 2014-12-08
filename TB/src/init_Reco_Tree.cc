8a33e6e88947f3b2f14daa13c224ebf86b93de08
Luca Brianza <brianza@hercules.mib.infn.it>
2014-12-08 10:39:05 +0100

    added number of hodoscope fibers ON

--- TB/src/init_Reco_Tree.cc
+++ TB/src/init_Reco_Tree.cc
@@ -1,5 +1,6 @@
 #include "../interface/init_Reco_Tree.h"
 
+int event;
 float                 time_CF[10];
 float                 time_CF_corr[10];
 float                 time_OT[10];
@@ -22,9 +23,10 @@ float                 amp_max_time[10];
 float                 charge[10];
 float                 charge_corr[10];
 float                 baseline[10];
-int                   fibreX[8];
+/*int                   fibreX[8];
 int                   fibreY[8];
 int                   sci_front_adc;
+*/
 int                   run_id;
 int                   isPCOn[10];
 int                   HV[10];
@@ -36,8 +38,13 @@ bool hodoX1[64];
 bool hodoY1[64];
 bool hodoX2[64];
 bool hodoY2[64];
+int nhodoX1;
+int nhodoX2;
+int nhodoY1;
+int nhodoY2;
 
 // List of branches                     
+TBranch *b_event;
 TBranch        *b_time_CF;   //! 
 TBranch        *b_time_CF_corr;   //!   
 TBranch        *b_time_OT;   //!     
@@ -60,9 +67,11 @@ TBranch        *b_amp_max_time;   //!
 TBranch        *b_baseline;   //!   
 TBranch        *b_charge;   //!    
 TBranch        *b_charge_corr;   //!  
+/*
 TBranch        *b_fibreX;   //!  
 TBranch        *b_fibreY;   //!  
 TBranch        *b_sci_front_adc;   //! 
+*/
 TBranch        *b_run_id;   //!   
 TBranch        *b_isPCOn;   //! 
 TBranch        *b_HV;   //!     
@@ -74,6 +83,10 @@ TBranch *b_hodoX1;
 TBranch *b_hodoY1;
 TBranch *b_hodoX2;
 TBranch *b_hodoY2;
+TBranch *b_nhodoX1;
+TBranch *b_nhodoY1;
+TBranch *b_nhodoX2;
+TBranch *b_nhodoY2;
 
 void init()
 {
@@ -82,6 +95,7 @@ void init()
 void SetOutTree(TTree* outTree)
 {
   //---standard analysis branches
+  outTree->Branch("event",&event,"event/I");
   outTree->Branch("time_CF",&time_CF,"time_CF[10]/F");
   outTree->Branch("time_CF_corr",&time_CF_corr,"time_CF_corr[10]/F");
   outTree->Branch("time_OT",&time_OT,"time_OT[10]/F");
@@ -106,11 +120,11 @@ void SetOutTree(TTree* outTree)
   outTree->Branch("baseline",&baseline,"baseline[10]/F");
   
   //---hodoscope branches       
-  outTree->Branch("fibreX",&fibreX,"fibreX[8]/I");
-  outTree->Branch("fibreY",&fibreY,"fibreY[8]/I");
+  //  outTree->Branch("fibreX",&fibreX,"fibreX[8]/I");
+  //  outTree->Branch("fibreY",&fibreY,"fibreY[8]/I");
   
   //---global branches    
-  outTree->Branch("sci_front_adc",&sci_front_adc,"sci_front_adc/I");
+  //  outTree->Branch("sci_front_adc",&sci_front_adc,"sci_front_adc/I");
   outTree->Branch("run_id",&run_id,"run_id/I");
 
   //---additional branches
@@ -126,12 +140,18 @@ void SetOutTree(TTree* outTree)
   outTree->Branch("hodoY1",&hodoY1,"hodoY1[64]/O");
   outTree->Branch("hodoX2",&hodoX2,"hodoX2[64]/O");
   outTree->Branch("hodoY2",&hodoY2,"hodoY2[64]/O");
+
+  outTree->Branch("nhodoX1",&nhodoX1,"nhodoX1/I");
+  outTree->Branch("nhodoY1",&nhodoY1,"nhodoY1/I");
+  outTree->Branch("nhodoX2",&nhodoX2,"nhodoX2/I");
+  outTree->Branch("nhodoY2",&nhodoY2,"nhodoY2/I");
   //    outTree->Branch("MCPName"+nameMCP->at(Ch_1),&baseline_Ch1,"baseline_"+nameMCP->at(Ch_1)+"/F");          
 }
 
    
 void InitRecoTree(TTree* nt)
 {
+  nt->SetBranchAddress("event", &event, &b_event);
   nt->SetBranchAddress("time_CF", &time_CF, &b_time_CF);
   nt->SetBranchAddress("time_CF_corr", &time_CF_corr, &b_time_CF_corr);
   nt->SetBranchAddress("time_OT", &time_OT, &b_time_OT);
@@ -154,9 +174,6 @@ void InitRecoTree(TTree* nt)
   nt->SetBranchAddress("charge", &charge, &b_charge);
   nt->SetBranchAddress("charge_corr", &charge_corr, &b_charge_corr);
   nt->SetBranchAddress("baseline", &baseline, &b_baseline);
-  nt->SetBranchAddress("fibreX", &fibreX, &b_fibreX);
-  nt->SetBranchAddress("fibreY", &fibreY, &b_fibreY);
-  nt->SetBranchAddress("sci_front_adc", &sci_front_adc, &b_sci_front_adc);
   nt->SetBranchAddress("run_id", &run_id, &b_run_id);
   nt->SetBranchAddress("isPCOn", &isPCOn, &b_isPCOn);
   nt->SetBranchAddress("HV", &HV, &b_HV);
@@ -168,4 +185,14 @@ void InitRecoTree(TTree* nt)
   nt->SetBranchAddress("hodoY1", &hodoY1, &b_hodoY1);
   nt->SetBranchAddress("hodoX2", &hodoX2, &b_hodoX2);
   nt->SetBranchAddress("hodoY2", &hodoY2, &b_hodoY2);
+  nt->SetBranchAddress("nhodoX1", &nhodoX1, &b_nhodoX1);
+  nt->SetBranchAddress("nhodoY1", &nhodoY1, &b_nhodoY1);
+  nt->SetBranchAddress("nhodoX2", &nhodoX2, &b_nhodoX2);
+  nt->SetBranchAddress("nhodoY2", &nhodoY2, &b_nhodoY2);
+
+  /*
+  nt->SetBranchAddress("fibreX", &fibreX, &b_fibreX);
+  nt->SetBranchAddress("fibreY", &fibreY, &b_fibreY);
+  nt->SetBranchAddress("sci_front_adc", &sci_front_adc, &b_sci_front_adc);
+  */
 }
