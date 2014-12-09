ce5a533570c277b768285392295f5d602ae67a68
amartelli <arabella.martelli@cern.ch>
2014-12-09 13:31:49 +0100

    trig saturated fraction

--- TB/script/runAllTime.sh
+++ TB/script/runAllTime.sh
@@ -5,23 +5,37 @@ python python/analyzer.py -cfg cfg/treshold_5s.txt -m enSEE -a time -n HVScan5 -
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack2 -a time -n HVScan6 -s HV
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m MiB3 -a time -n HVScan7 -s HV
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack1 -a time -n HVScan8 -s HV
+
+python python/analyzer.py -cfg cfg/treshold_5s.txt -m MiB2 -a time -n scanX0_HVHigh50GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m enSEE -a time -n scanX0_HVHigh50GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack1 -a time -n scanX0_HVHigh50GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack2 -a time -n scanX0_HVHigh50GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m MiB3 -a time -n scanX0_HVHigh50GeV -s X0
+
+python python/analyzer.py -cfg cfg/treshold_5s.txt -m MiB2 -a time -n scanX0_HVHigh20GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m enSEE -a time -n scanX0_HVHigh20GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack1 -a time -n scanX0_HVHigh20GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack2 -a time -n scanX0_HVHigh20GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m MiB3 -a time -n scanX0_HVHigh20GeV -s X0
+
+python python/analyzer.py -cfg cfg/treshold_5s.txt -m MiB2 -a time -n scanX0_HVLow20GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m enSEE -a time -n scanX0_HVLow20GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack1 -a time -n scanX0_HVLow20GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack2 -a time -n scanX0_HVLow20GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m MiB3 -a time -n scanX0_HVLow20GeV -s X0
+
+python python/analyzer.py -cfg cfg/treshold_5s.txt -m MiB2 -a time -n scanX0_HVLow50GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m enSEE -a time -n scanX0_HVLow50GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack1 -a time -n scanX0_HVLow50GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack2 -a time -n scanX0_HVLow50GeV -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m MiB3 -a time -n scanX0_HVLow50GeV -s X0
+
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m enSEE -a time -n AngScan_HVLow -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack1 -a time -n AngScan_HVLow -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack2 -a time -n AngScan_HVLow -s X0
 python python/analyzer.py -cfg cfg/treshold_5s.txt -m MiB3 -a time -n AngScan_HVLow -s X0
+
+python python/analyzer.py -cfg cfg/treshold_5s.txt -m enSEE -a time -n AngScan_HVHigh -s X0
+python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack1 -a time -n AngScan_HVHigh -s X0
+python python/analyzer.py -cfg cfg/treshold_5s.txt -m ZStack2 -a time -n AngScan_HVHigh -s X0
+python python/analyzer.py -cfg cfg/treshold_5s.txt -m MiB3 -a time -n AngScan_HVHigh -s X0
