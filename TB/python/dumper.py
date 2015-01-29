05188ac2ad01f148b384fb4a0fcec89522290a86
Luca Brianza <brianza@hercules.mib.infn.it>
2015-01-29 17:47:17 +0100

    saving clock info into ntuples

--- TB/python/dumper.py
+++ TB/python/dumper.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser (description = 'dumper: create reco tree from raw data')
     parser.add_argument ('-cfg', '--cfg' , default = 'cfg/HVScan1.cfg' , help='list of run')
     parser.add_argument ('-d', '--dir' , default = '/gwteray/users/brianza/iMCP_H4', help='directory containing raw data')
-    parser.add_argument ('-c', '--chNumber' , default = '10', help='number of channels (in the raw data!)')
+    parser.add_argument ('-c', '--chNumber' , default = '11', help='number of channels (in the raw data!)')
     parser.add_argument ('-n', '--name' , default = 'HVScan1', help='suffix of reco output file')
     args = parser.parse_args ()
     print 'dumper '+args.cfg+' '+args.dir+' '+args.chNumber+' '+args.name
