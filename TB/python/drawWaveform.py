40f5cbd9d31226ea8a95bbfd4482e6ea83f9465d
Luca Brianza <brianza@hercules.mib.infn.it>
2015-05-08 15:54:35 +0200

    removed the all hard-coded dependencies (trigger positions, etc..

--- TB/python/drawWaveform.py
+++ TB/python/drawWaveform.py
@@ -13,7 +13,8 @@ if __name__ == '__main__':
     parser.add_argument ('-c', '--channel' , default = '4', help='channel to analyze')
     parser.add_argument ('-f', '--firstEntry' , default = '0', help='first entry')
     parser.add_argument ('-n', '--nEvents' , default = '100', help='number of events to analyze')
+    parser.add_argument ('-tP', '--trigPos' , default = '4', help='position (channel) of the trigger in the raw files')
     args = parser.parse_args ()
-    print './drawWaveform '+args.dir+' '+args.run+' '+args.channel+' '+args.firstEntry+' '+args.nEvents
-    os.system('./drawWaveform.exe '+args.dir+' '+args.run+' '+args.channel+' '+args.firstEntry+' '+args.nEvents)
+    print './drawWaveform '+args.dir+' '+args.run+' '+args.channel+' '+args.firstEntry+' '+args.nEvents+' '+args.trigPos
+    os.system('./drawWaveform.exe '+args.dir+' '+args.run+' '+args.channel+' '+args.firstEntry+' '+args.nEvents+' '+args.trigPos)
     
