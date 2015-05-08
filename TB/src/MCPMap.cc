88e183d634e3a3bf1bea149d8afa8947880e5ded
Luca Brianza <brianza@hercules.mib.infn.it>
2015-05-08 15:54:45 +0200

    removed the all hard-coded dependencies (trigger positions, etc..

--- TB/src/MCPMap.cc
+++ TB/src/MCPMap.cc
@@ -14,8 +14,15 @@ void Fill_MCPList (void)
       MCPList.insert(std::make_pair("Off2",6));
       MCPList.insert(std::make_pair("Off3",7));
       MCPList.insert(std::make_pair("Off4",8));
-      MCPList.insert(std::make_pair("trig1",9));
-      MCPList.insert(std::make_pair("trig2",10));
+      MCPList.insert(std::make_pair("clock1",9));
+      MCPList.insert(std::make_pair("clock2",10));
+      MCPList.insert(std::make_pair("Off6",11));
+      MCPList.insert(std::make_pair("Off7",12));
+      MCPList.insert(std::make_pair("Off8",13));
+      MCPList.insert(std::make_pair("Off9",14));
+      MCPList.insert(std::make_pair("Off10",15));
+      MCPList.insert(std::make_pair("Off11",16));
+      MCPList.insert(std::make_pair("Off12",17));
     }
 
 void Fill_inverted_MCPList (void)
@@ -29,6 +36,13 @@ void Fill_inverted_MCPList (void)
       inverted_MCPList.insert(std::make_pair(6,"Off2"));
       inverted_MCPList.insert(std::make_pair(7,"Off3"));
       inverted_MCPList.insert(std::make_pair(8,"Off4"));
-      inverted_MCPList.insert(std::make_pair(9,"trig1"));
-      inverted_MCPList.insert(std::make_pair(10,"trig2"));
+      inverted_MCPList.insert(std::make_pair(9,"clock1"));
+      inverted_MCPList.insert(std::make_pair(10,"clock2"));
+      inverted_MCPList.insert(std::make_pair(11,"Off6"));
+      inverted_MCPList.insert(std::make_pair(12,"Off7"));
+      inverted_MCPList.insert(std::make_pair(13,"Off8"));
+      inverted_MCPList.insert(std::make_pair(14,"Off9"));
+      inverted_MCPList.insert(std::make_pair(15,"Off10"));
+      inverted_MCPList.insert(std::make_pair(16,"Off11"));
+      inverted_MCPList.insert(std::make_pair(17,"Off12"));
     }
