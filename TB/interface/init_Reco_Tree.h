332947874f4079bae52cef1bd29b09b0f8410429
Luca Brianza <brianza@hercules.mib.infn.it>
2015-05-08 15:54:30 +0200

    removed the all hard-coded dependencies (trigger positions, etc..

--- TB/interface/init_Reco_Tree.h
+++ TB/interface/init_Reco_Tree.h
@@ -6,33 +6,33 @@
 
 // Declaration of leaf types 
 extern int                 event;
-extern float                 time_CF[11];
-extern float                 time_CF_corr[11];
-extern float                 time_CF30[11];
-extern float                 time_OT[11];
-extern float                 time_start[11];
-extern float                 time_stop[11];
-extern float                 time_start_1000[11];
-extern float                 time_stop_1000[11];
-extern float                 time_start_150[11];
-extern float                 time_stop_150[11];
-extern float                 time_start_200[11];
-extern float                 time_stop_200[11];
-extern float                 time_start_500[11];
-extern float                 time_stop_500[11];
-extern float                 time_start_300[11];
-extern float                 time_stop_300[11];
-extern float                 time_Max[11];
-extern float                 amp_max[11];
-extern float                 amp_max_corr[11];
-extern float                 amp_max_time[11];
-extern float                 charge[11];
-extern float                 charge_corr[11];
-extern float                 baseline[11];
+extern float                 time_CF[18];
+extern float                 time_CF_corr[18];
+extern float                 time_CF30[18];
+extern float                 time_OT[18];
+extern float                 time_start[18];
+extern float                 time_stop[18];
+extern float                 time_start_1000[18];
+extern float                 time_stop_1000[18];
+extern float                 time_start_150[18];
+extern float                 time_stop_150[18];
+extern float                 time_start_200[18];
+extern float                 time_stop_200[18];
+extern float                 time_start_500[18];
+extern float                 time_stop_500[18];
+extern float                 time_start_300[18];
+extern float                 time_stop_300[18];
+extern float                 time_Max[18];
+extern float                 amp_max[18];
+extern float                 amp_max_corr[18];
+extern float                 amp_max_time[18];
+extern float                 charge[18];
+extern float                 charge_corr[18];
+extern float                 baseline[18];
 extern int                   run_id;
-extern int                   isPCOn[11];
-extern int                   HV[11];
-extern int                   isTrigger[11];
+extern int                   isPCOn[18];
+extern int                   HV[18];
+extern int                   isTrigger[18];
 extern float                 X0;
 extern float tdcX;
 extern float tdcY;
@@ -44,10 +44,10 @@ extern int nhodoX1;
 extern int nhodoX2;
 extern int nhodoY1;
 extern int nhodoY2;
+extern int                   sci_front_adc;
 /*
 extern int                   fibreX[8];
 extern int                   fibreY[8];
-extern int                   sci_front_adc;
 */
 
 // List of branches                 
@@ -90,10 +90,10 @@ extern TBranch *b_nhodoX1;
 extern TBranch *b_nhodoY1;
 extern TBranch *b_nhodoX2;
 extern TBranch *b_nhodoY2;
+extern TBranch        *b_sci_front_adc;   //!                               
 /*
 extern TBranch        *b_fibreX;   //!                                    
 extern TBranch        *b_fibreY;   //!                                        
-extern TBranch        *b_sci_front_adc;   //!                               
 */
 
 void InitRecoTree(TTree* nt);
