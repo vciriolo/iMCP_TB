b16396f360df1f52c8f80efbe14c0f0ad5fa47bc
Luca Brianza <brianza@hercules.mib.infn.it>
2015-05-11 16:55:56 +0200

    new MCP list for BTF 2015

--- src/MCPMap.cc
+++ src/MCPMap.cc
@@ -6,16 +6,16 @@ std::map<int,std::string> inverted_MCPList;
 void Fill_MCPList (void)
     {
       MCPList.insert(std::make_pair("MiB2",0));
-      MCPList.insert(std::make_pair("ZStack1",1));
-      MCPList.insert(std::make_pair("enSEE",2));
-      MCPList.insert(std::make_pair("ZStack2",3));
-      MCPList.insert(std::make_pair("MiB3",4));
-      MCPList.insert(std::make_pair("Off1",5));
-      MCPList.insert(std::make_pair("Off2",6));
-      MCPList.insert(std::make_pair("Off3",7));
-      MCPList.insert(std::make_pair("Off4",8));
-      MCPList.insert(std::make_pair("clock1",9));
-      MCPList.insert(std::make_pair("clock2",10));
+      MCPList.insert(std::make_pair("GaAsEm",1));
+      MCPList.insert(std::make_pair("MultiAlkEm",2));
+      MCPList.insert(std::make_pair("Double9090",3));
+      MCPList.insert(std::make_pair("Double4040",4));
+      MCPList.insert(std::make_pair("clock1",5));
+      MCPList.insert(std::make_pair("clock2",6));
+      MCPList.insert(std::make_pair("Off1",7));
+      MCPList.insert(std::make_pair("Off2",8));
+      MCPList.insert(std::make_pair("Off3",9));
+      MCPList.insert(std::make_pair("Off4",10));
       MCPList.insert(std::make_pair("Off6",11));
       MCPList.insert(std::make_pair("Off7",12));
       MCPList.insert(std::make_pair("Off8",13));
@@ -28,16 +28,16 @@ void Fill_MCPList (void)
 void Fill_inverted_MCPList (void)
     {
       inverted_MCPList.insert(std::make_pair(0,"MiB2"));
-      inverted_MCPList.insert(std::make_pair(1,"ZStack1"));
-      inverted_MCPList.insert(std::make_pair(2,"enSEE"));
-      inverted_MCPList.insert(std::make_pair(3,"ZStack2"));
-      inverted_MCPList.insert(std::make_pair(4,"MiB3"));
-      inverted_MCPList.insert(std::make_pair(5,"Off1"));
-      inverted_MCPList.insert(std::make_pair(6,"Off2"));
-      inverted_MCPList.insert(std::make_pair(7,"Off3"));
-      inverted_MCPList.insert(std::make_pair(8,"Off4"));
-      inverted_MCPList.insert(std::make_pair(9,"clock1"));
-      inverted_MCPList.insert(std::make_pair(10,"clock2"));
+      inverted_MCPList.insert(std::make_pair(1,"GaAsEm"));
+      inverted_MCPList.insert(std::make_pair(2,"MultiAlkEm"));
+      inverted_MCPList.insert(std::make_pair(3,"Double9090"));
+      inverted_MCPList.insert(std::make_pair(4,"Double9040"));
+      inverted_MCPList.insert(std::make_pair(5,"clock1"));
+      inverted_MCPList.insert(std::make_pair(6,"clock2"));
+      inverted_MCPList.insert(std::make_pair(7,"Off1"));
+      inverted_MCPList.insert(std::make_pair(8,"Off2"));
+      inverted_MCPList.insert(std::make_pair(9,"Off3"));
+      inverted_MCPList.insert(std::make_pair(10,"Off4"));
       inverted_MCPList.insert(std::make_pair(11,"Off6"));
       inverted_MCPList.insert(std::make_pair(12,"Off7"));
       inverted_MCPList.insert(std::make_pair(13,"Off8"));
