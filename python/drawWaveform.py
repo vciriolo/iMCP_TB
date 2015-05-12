d556245a2bbee9def345ece7588a103f89d3c8d4
Luca Brianza <brianza@hercules.mib.infn.it>
2015-05-12 15:38:35 +0200

    drawWaveform modified for BTF data

--- python/drawWaveform.py
+++ python/drawWaveform.py
@@ -9,10 +9,10 @@ import string
 if __name__ == '__main__':
     parser = argparse.ArgumentParser (description = 'draw waveform: read raw data and draw some waveforms')
     parser.add_argument ('-d', '--dir' , default = '/gwteray/users/pigazzini/IMCP/BTF2015', help='directory containing raw data')
-    parser.add_argument ('-r', '--run' , default = '1820', help='run to scan')
-    parser.add_argument ('-c', '--channel' , default = '4', help='channel to analyze')
-    parser.add_argument ('-f', '--firstEntry' , default = '0', help='first entry')
-    parser.add_argument ('-n', '--nEvents' , default = '100', help='number of events to analyze')
+    parser.add_argument ('-r', '--run' , default = '1834', help='run to scan')
+    parser.add_argument ('-c', '--channel' , default = '1', help='channel to analyze')
+    parser.add_argument ('-f', '--firstEntry' , default = '11', help='first entry')
+    parser.add_argument ('-n', '--nEvents' , default = '1', help='number of events to analyze')
     parser.add_argument ('-tP', '--trigPos' , default = '4', help='position (channel) of the trigger in the raw files')
     args = parser.parse_args ()
     print './drawWaveform '+args.dir+' '+args.run+' '+args.channel+' '+args.firstEntry+' '+args.nEvents+' '+args.trigPos
