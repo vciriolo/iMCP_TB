d201be0835684667acc155cc55cd92afa4349c13
Luca Brianza <brianza@hercules.mib.infn.it>
2015-05-12 14:57:33 +0200

    drawWaveform modified for BTF data

--- python/drawWaveform.py
+++ python/drawWaveform.py
@@ -8,8 +8,8 @@ import argparse
 import string
 if __name__ == '__main__':
     parser = argparse.ArgumentParser (description = 'draw waveform: read raw data and draw some waveforms')
-    parser.add_argument ('-d', '--dir' , default = '/gwteray/users/brianza/iMCP_H4', help='directory containing raw data')
-    parser.add_argument ('-r', '--run' , default = '744', help='run to scan')
+    parser.add_argument ('-d', '--dir' , default = '/gwteray/users/pigazzini/IMCP/BTF2015', help='directory containing raw data')
+    parser.add_argument ('-r', '--run' , default = '1820', help='run to scan')
     parser.add_argument ('-c', '--channel' , default = '4', help='channel to analyze')
     parser.add_argument ('-f', '--firstEntry' , default = '0', help='first entry')
     parser.add_argument ('-n', '--nEvents' , default = '100', help='number of events to analyze')
