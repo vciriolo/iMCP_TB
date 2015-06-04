51fbf92832609a273a52fb7f27791d2aa85bf393
Luca Brianza <brianza@hercules.mib.infn.it>
2015-06-04 11:11:28 +0200

    update script

--- script/runAllAnalysis.sh
+++ script/runAllAnalysis.sh
@@ -1,6 +1,4 @@
 sh script/runAllEff.sh
 sh script/runAllTimeCFD.sh
-sh script/runAllTimeLED.sh
 python python/drawEfficiency.py
 python python/drawResolutionCFD.py
-python python/drawResolutionLED.py
